b31701e2ca28cc08a26ed0038324b9eb5788c42e
Daffa Fauzul Hakim <94423743+DaffaFauz@users.noreply.github.com>
2022-07-12 21:19:50 +0700

    Update script.js

--- finalSubmission/assets/script.js
+++ finalSubmission/assets/script.js
@@ -1,14 +1,1 @@
-var nav = document.querySelector('.navbar');
-var make = document.createElement('div');
-make.className = 'hamburger';
-make.innerHTML = "<span class='bar'></span>";
-make.innerHTML += "<span class='bar'></span>";
-make.innerHTML += "<span class='bar'></span>";
-nav.appendChild(make);
-
-const hamburger = document.querySelector('.hamburger');
-const menu = document.querySelector('.menu');
-hamburger.addEventListener('click', function () {
-  hamburger.classList.toggle('active');
-  menu.classList.toggle('active');
-});
+var nav=document.querySelector(".navbar"),make=document.createElement("div");make.className="hamburger",make.innerHTML="<span class='bar'></span>",make.innerHTML+="<span class='bar'></span>",make.innerHTML+="<span class='bar'></span>",nav.appendChild(make);const hamburger=document.querySelector(".hamburger"),menu=document.querySelector(".menu");hamburger.addEventListener("click",(function(){hamburger.classList.toggle("active"),menu.classList.toggle("active")}));
